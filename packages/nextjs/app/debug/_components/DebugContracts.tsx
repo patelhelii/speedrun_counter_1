2a16d28a90b032ed12711817c9fd65ceeba963d3
patelhelii <patelheli0710@gmail.com>
2025-08-02 07:05:57 +0000

    initial submission

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { ICounter } from "./ICounter";
 import { ethers } from "ethers";
 
-const contractAddress = "0x664508a7178451da209b9387f671c347eb28c0cc";
+const contractAddress = "0x57f9041d3f205f77fc1ad5e6274fcc316d2c0836";
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
